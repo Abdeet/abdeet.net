0868069c3bb865eadacfbe9e0145ce3d32b98f78
Abdeet <senthilkumar.abhi@gmail.com>
2022-06-29 01:42:38 -0400

    going crazy x2

--- images/site/__init__.py
+++ images/site/__init__.py
@@ -51,8 +51,8 @@ def create_app(test_config=None):
     def create_thumbnail(album_id, image_filename):
         path_to_full_size = os.path.join(app.static_folder, f"albums/{album_id}")
         path_to_thumbnails = os.path.join(app.static_folder, f"albums/{album_id}/thumbnails")
-        image_name = os.path.splitext(image_filename[0])[0]
-        image_ext = os.path.splitext(image_filename[0])[1]
+        image_name = os.path.splitext(image_filename)[0]
+        image_ext = os.path.splitext(image_filename)[1]
         image = Image.open(os.path.join(path_to_full_size, image_filename))
         full_width, full_height = image.size
         new_width, new_height = 500 * full_width / full_height, 500
@@ -61,8 +61,10 @@ def create_app(test_config=None):
 
     def get_thumbnail(album_id, image_filename):
         path_to_thumbnails = os.path.join(app.static_folder, f"albums/{album_id}/thumbnails")
-        image_name = os.path.splitext(image_filename[0])[0]
-        image_ext = os.path.splitext(image_filename[0])[1]
+        image_name="test"
+        image_ext=".jpg"
+        #image_name = os.path.splitext(image_filename[0])[0]
+        #image_ext = os.path.splitext(image_filename[0])[1]
         if not os.path.exists(path_to_thumbnails):
             os.makedirs(path_to_thumbnails)
         if not os.path.exists(os.path.join(path_to_thumbnails, f"{image_name}_thumbnail{image_ext}")):
